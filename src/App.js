a1e7c69d007093c1bd1d53ae446d9c5c5090f841
Jakob <jako2889@stud.kea.dk>
2019-05-18 16:39:55 +0200

    added modal to step1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 // import Site from "./components/site.jsx";
 import Nav from "./components/nav.jsx";
+import Step1 from "./components/Step1";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Step1 />
     </div>
   );
 }
