8342178d967b0b510523134e86f178d67f02eb7b
Jakob <jako2889@stud.kea.dk>
2019-05-18 21:56:50 +0200

    finished middle column

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 // import Site from "./components/site.jsx";
 import Nav from "./components/nav.jsx";
 import Step1 from "./components/Step1";
+import Right_Column from "./components/Right_Column.jsx";
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Step1 />
+      <Right_Column />
     </div>
   );
 }
---
+++ src/components/Right_Column.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import "../css/Right_Column.scss";
+import starIcon from "../assets/star.svg";
+
+export class Right_Column extends Component {
+  render() {
+    return (
+      <div className="mid_wrapper">
+        <div className="mid_con">
+            <p>Forside > Esport > Counter-Strike GO > ESL Pro League</p>
+            <div className="mid_header">
+                <p className="title">ESL Pro League</p>
+                <div className="favourites">
+                <p>Tilføj til mine favoritter</p>
+                <img src={starIcon} alt="A star icon"/>
+                </div>
+            </div>
+            <div className="mid_content">
+                <div className="time">
+                    <label htmlFor="time">Tid</label>
+                    <select name="time">
+                        <option value="all">Alle</option>
+                        <option value="today">Idag</option>
+                        <option value="next4hours">Næste 4 timer</option>
+                        <option value="next3days">Næste 3 dage</option>
+                    </select>
+                </div>
+                <div className="bet">
+                <label htmlFor="bet">Væddemål</label>
+                    <select name="bet">
+                        <option value="all">Alle kampe</option>
+                        <option value="matchwinner">Kampvinder</option>
+                        <option value="firsthalf">Hvem vinder første halvleg?</option>
+                        <option value="secondhalf">Hvem vinder anden halvleg</option>
+                        <option value="firstgoal">Hvem scorer først?</option>
+                        <option value="bothteamscores">Begge hold scorer? Ja/Nej</option>
+                    </select>
+                </div>
+                <div className="streaming">
+                <label htmlFor="streaming">Tv</label>
+                    <select name="streaming">
+                        <option value="tv">Tv</option>
+                        <option value="danskespil">Danske Spil Tv</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+        
+      </div>
+    )
+  }
+}
+
+export default Right_Column
