228d2a69c5b3a83b494f3781d8b3087a9031ca91
Jakob <jako2889@stud.kea.dk>
2019-05-21 01:50:23 +0200

    Added input etc elements in acc details & styled form

--- src/components/FormAccountDetails.jsx
+++ src/components/FormAccountDetails.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import "../css/FormStyle.scss";
 
 export class FormAccountDetails extends Component {
     continue = e => {
@@ -8,11 +9,49 @@ export class FormAccountDetails extends Component {
     render() {
         // Make values a variable to destruct and just use the variable instead of props
         const { values, handleChange } = this.props;
-        return (
-                    <React.Fragment>
-                    <h1>Form Account Details</h1>    
-                    <button onClick={this.continue}>Continue</button>
-                    </React.Fragment>
+        return (  
+            <form>
+                <h1>Form Account Details</h1>
+                
+                <div className="inputStyle">
+                <input type="email" placeholder="Email"
+                 onChange={handleChange("email")}
+                 defaultValue={values.email}
+                 required
+                 ></input> 
+                <label>Email</label>
+                <span className="helpText"></span>
+                 </div>
+
+                <div className="inputStyle">
+                <input type="text" placeholder="Brugernavn"
+                 onChange={handleChange("userName")}
+                 defaultValue={values.userName}
+                 required
+                 ></input> 
+                <label>Brugernavn</label> 
+                </div>
+
+                <div className="inputStyle">
+                <input type="password" placeholder="Adgangskode"
+                 onChange={handleChange("password")}
+                 defaultValue={values.password}
+                 required
+                 ></input> 
+                 <label>Adgangskode</label> 
+                 </div>
+
+                <div className="inputStyle">
+                <input type="password" placeholder="Indtast adgangskode igen"
+                 onChange={handleChange("confirmPassword")}
+                 defaultValue={values.confirmPassword}
+                 required
+                 ></input> 
+                 <label>Indtast adgangskode igen</label>
+                 </div>      
+                <button onClick={this.continue}>Continue</button>
+
+            </form>
         )
     }
 }
