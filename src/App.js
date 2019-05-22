8bec2ecc39494af500382a50ad37c3acec7bf285
Jakob <jako2889@stud.kea.dk>
2019-05-22 13:55:51 +0200

    changed site to be ready for merge

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import Nav from "./components/nav.jsx";
 import Step1 from "./components/Step1";
 import Mid_Column from "./components/Mid_Column.jsx";
 import FormBeginFlow from "./components/FormBeginFlow.jsx";
-import GameModal from "./components/GameModal";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <div id="site_content_wrapper">
-         <GameModal /> 
+         <Site /> 
       </div>
     </div>
   );
