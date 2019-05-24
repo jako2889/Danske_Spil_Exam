580d3797699ac0d153a0fccf445215766b0828b3
Magnus V Jensen <magnusvjensen@gmail.com>
2019-05-24 14:41:44 +0200

    ladning page update

--- src/components/Step1.jsx
+++ src/components/Step1.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "../css/Step1.scss";
-import Step1Image from "../assets/step1_image.svg";
-
+// import Step1Image from "../assets/step1_image.svg";
+import ds_logo from "../assets/ds_logo.svg";
 export class Step1 extends Component {
   continue = e => {
     e.preventDefault();
@@ -12,21 +12,33 @@ export class Step1 extends Component {
     return (
       <div className="Modal_wrapper">
         <div className="Modal_con">
-          <div className="Modal_text">
-            <h1>Velkommen</h1>
-            <p>
-              Hos Danske Spil tilbyder vi en underholdende &amp; sikker spil
-              oplevelse. Vi vil gerne lige vise dig, hvordan du bedst vinder med
-              dine yndlings hold.
-            </p>
-            <div className="Button_con">
-              <button className="Modal_button" onClick={this.continue}>
-                Start Tutorial
-              </button>
-              <div className="button_tip" />
+          <div className="lp_logo_wrap">
+            <div className="lp_logo_con">
+              <img src={ds_logo} alt="Danske spil logo" />
             </div>
           </div>
-          <img src={Step1Image} alt="Popular Games" />
+          <div className="lp_txt_con">
+            <div className="lp_header">Velkommen</div>
+            <div className="lp_l1">
+              Mærk spændeingen ved at bette når du spiller på dine yndlings
+              spil.
+            </div>
+            <div className="lp_l2">
+              Gennemfør vores hurtige totutial og vind en velkomst bonus på
+              <span className="lp_span"> 250kr.</span>
+            </div>
+          </div>
+          <div className="lp_btn_wrap">
+            <div className="lp_btn_con">
+              <button
+                className="lp_btn"
+                onClick={this.continue}
+                style={{ cursor: "pointer" }}
+              >
+                Begin
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     );
@@ -34,3 +46,20 @@ export class Step1 extends Component {
 }
 
 export default Step1;
+
+//         <div className="Modal_text">
+{
+  /* <h1>Velkommen</h1>
+<p>
+  Hos Danske Spil tilbyder vi en underholdende &amp; sikker spil
+  oplevelse. Vi vil gerne lige vise dig, hvordan du bedst vinder med
+  dine yndlings hold.
+</p>
+<div className="Button_con">
+  <button className="Modal_button" onClick={this.continue}>
+    Start Tutorial
+  </button>
+  <div className="button_tip" />
+</div>
+</div> */
+}
